a138ed805ecd6007f6d443473e5ca726b171adde
Circuit Specialists <github@circuitspecialists.com>
2019-11-04 13:56:13 -0700

    update

--- ESP/main.py
+++ ESP/main.py
@@ -13,9 +13,9 @@ class MAIN:
         self.device = espressif.ESP8266()
         self.PULSE = Pin(self.device.D1, Pin.OUT)
         self.DIR = Pin(self.device.D2, Pin.OUT)
-        self.setStepsPerRevolution(200) # Out motors are 200 steps per volution
+        self.setStepsPerRevolution(200.00) # Out motors are 200 steps per volution
         self.setDirection('CW')
-        self.setSpeed(300) # default is 300 RPM for fluid motion
+        self.setSpeed(300.00) # default is 300 RPM for fluid motion
 
     def setStepsPerRevolution(self, steps):
         self.steps_per_revolution = steps
@@ -29,19 +29,16 @@ class MAIN:
             return 'not a valid direction'
 
     def setSpeed(self, speed):
-        # speed = RPM
         self.speed = float(speed)
 
     def rotate(self, degrees, direction=None):
         if(direction != None):
             self.setDirection(direction)
-        steps = degrees / (360.0 / self.steps_per_revolution)
+        steps = degrees / (360.00 / self.steps_per_revolution)
         self.step(steps)
 
     def step(self, steps):
         delayTime = 150000.00 / self.speed
-        # use time base, such as sleep_us = 1000000
-        # must use int for ms or us
         for step in range(int(steps)):
             self.PULSE.on()
             time.sleep_us(20)
